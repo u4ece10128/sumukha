0f4465c3573646c3c2b8f1ab48c66be6ed3e46c0
jayaram.kollipara <jayaram.kollipara@igenius.ai>
2020-09-21 17:33:48 +0200

    add: implement CCA between trained and general embeddings

--- src/sumukha/encoding/build_encoders.py
+++ src/sumukha/encoding/build_encoders.py
@@ -1,6 +1,11 @@
 import fasttext as ft
+
 import numpy as np
+
 from brushes.io import filesystem
+
+from sklearn.cross_decomposition import CCA
+
 from sumukha.config import input_preprocess_path
 
 
@@ -55,3 +60,48 @@ def domain_encoders(preprocess_path, trained_embeddings_path):
     filesystem.save_obj(vocab, preprocess_path, 'domain_vocab_processed')
     filesystem.save_obj(vectors, preprocess_path, 'domain_vector_processed')
 
+
+def mixing_embeddings(preprocess_path, embeddings_path_domain_adapted):
+    """
+    This method performs Canonical Correlational Analysis on the common
+    vocabulary words
+    :param preprocess_path:
+    :param embeddings_path_domain_adapted
+    :return:
+    """
+    # Read Vocabulary.
+    general_vocabs = filesystem.load_obj(preprocess_path, 'general_vocab_processed')
+    general_vectors = filesystem.load_obj(preprocess_path, 'general_vector_processed')
+
+    domain_vocabs = filesystem.load_obj(preprocess_path, 'domain_vocab_processed')
+    domain_vectors = filesystem.load_obj(preprocess_path, 'domain_vector_processed')
+
+    intersection_vocab = dict()
+    for vocab in general_vocabs:
+        if vocab in domain_vocabs:
+            intersection_vocab[vocab] = [general_vocabs[vocab], domain_vocabs[vocab]]
+
+    print('Number of General Related Vocabularies: ', len(general_vocabs))
+    print('Number of Domain Related Vocabularies: ', len(domain_vocabs))
+    print('Number of Common Vocabularies: ', len(intersection_vocab))
+
+    mixed_embeddings = {}
+
+    for common_vocab in intersection_vocab:
+        vecotr_g = general_vectors[general_vocabs[common_vocab]]
+        vector_ds = domain_vectors[domain_vocabs[common_vocab]]
+
+        cca = CCA(n_components=1)
+        cca.fit(vecotr_g.reshape(-1, 1), vector_ds.reshape(-1, 1))
+        projection_g, projection_ds = cca.transform(vecotr_g.reshape(-1, 1), vector_ds.reshape(-1, 1))
+
+        # alpha and beta can be tuned
+        alpha = 0.5
+        beta = 0.5
+
+        wg_hat = vecotr_g * projection_g.T
+        wds_hat = vector_ds * projection_ds.T
+        wda = (alpha * wg_hat) + (beta * wds_hat)
+        mixed_embeddings[common_vocab] = wda
+
+    filesystem.save_obj(mixed_embeddings, embeddings_path_domain_adapted, 'domain_adapted_embeddings')
